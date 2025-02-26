dada1e799fd264f9ca002218185a2facef7779c1
Przemysław Wilary <przemyslaw.wilary@gmail.com>
2025-02-26 13:42:15 +0100

    streams added

--- src/io/ConsolePrinter.java
+++ src/io/ConsolePrinter.java
@@ -10,35 +10,32 @@ import java.util.Collection;
 public class ConsolePrinter {
 
     public void printBooks(Collection<Publication> publications){
-        int countBooks = 0;
-        for (Publication publication : publications) {
-            if (publication instanceof Book){
-                printLine(publication.toString());
-                countBooks++;
-            }
-        }
-        if (countBooks == 0) {
+        long count = publications.stream()
+                .filter(p-> p instanceof Book)
+                .map(Publication::toString)
+                .peek(this::printLine)
+                .count();
+        if (count == 0) {
             printLine("Brak książek w bibliotece");
         }
+
     }
 
     public void printMagazines(Collection<Publication> publications){
-        int countMagazines = 0;
-        for (Publication publication : publications) {
-            if(publication instanceof Magazine){
-                printLine(publication.toString());
-            }
-            countMagazines++;
-        }
-        if (countMagazines == 0) {
+        long count = publications.stream()
+                .filter(p-> p instanceof Magazine)
+                .map(Publication::toString)
+                .peek(this::printLine)
+                .count();
+        if (count == 0) {
             printLine("Brak magazynów w bibliotece");
         }
     }
 
     public void printUsers(Collection<LibraryUser> users){
-        for (LibraryUser user : users) {
-            printLine(user.toString());
-        }
+        users.stream()
+                .map(LibraryUser::toString)
+                .forEach(this::printLine);
     }
 
     public void printLine(String text){
